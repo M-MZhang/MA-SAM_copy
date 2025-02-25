fabb0598ae23f4e461f39ab0f544658170569d6b
M-MZhang <2211305270@qq.com>
2025-02-25 10:44:37 +0800

    train.py

--- MA-SAM/train.py
+++ MA-SAM/train.py
@@ -21,7 +21,7 @@ parser.add_argument('--root_path', type=str, default='/root/data1/zmm/seg4medici
 parser.add_argument('--output', type=str, default='/root/data1/zmm/seg4medicine/save/Vanille_me_v5.22')
 parser.add_argument('--data_path', type=str, default='/root/data1/zmm/seg4medicine/data/BTCV')
 parser.add_argument('--num_classes', type=int, default=12, help='output channel of network')
-parser.add_argument('--batch_size', type=int, default=6, help='batch_size per gpu')
+parser.add_argument('--batch_size', type=int, default=7, help='batch_size per gpu')
 parser.add_argument('--n_gpu', type=int, default=8, help='total gpu')
 parser.add_argument('--base_lr', type=float, default=0.0008, help='segmentation network learning rate')
 
@@ -41,7 +41,7 @@ parser.add_argument('--warmup_period', type=int, default=250, help='Warp up iter
 parser.add_argument('--AdamW', action='store_true', help='If activated, use AdamW to finetune SAM model')
 parser.add_argument('--module', type=str, default='task_specific_sam')
 parser.add_argument('--dice_param', type=float, default=0.8)
-parser.add_argument('--lr_exp', type=float, default=5, help='The learning rate decay expotential')
+parser.add_argument('--lr_exp', type=float, default=4, help='The learning rate decay expotential')
 
 # acceleration choices
 parser.add_argument('--tf32', action='store_true', help='If activated, use tf32 to accelerate the training process')
