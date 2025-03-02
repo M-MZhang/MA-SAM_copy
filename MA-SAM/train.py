bfdc04bb79fdb6b4d4bac1a42dff365b807955ae
M-MZhang <2211305270@qq.com>
2025-03-02 22:39:17 +0800

    runing

--- MA-SAM/train.py
+++ MA-SAM/train.py
@@ -18,7 +18,7 @@ import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--root_path', type=str, default='/root/autodl-tmp/RawData/Training', help='root dir for data')
-parser.add_argument('--output', type=str, default='/root/autodl-tmp/save/Vanille_me_v6.7')
+parser.add_argument('--output', type=str, default='/root/autodl-tmp/save/Vanille_me_v8')
 parser.add_argument('--data_path', type=str, default='/root/autodl-tmp/RawData/Training')
 parser.add_argument('--num_classes', type=int, default=12, help='output channel of network')
 parser.add_argument('--batch_size', type=int, default=20, help='batch_size per gpu')
