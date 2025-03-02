a096331c3e247cbceebf2b20be39896664c6ceb3
M-MZhang <2211305270@qq.com>
2025-03-02 09:34:44 +0800

    runnning

--- MA-SAM/train.py
+++ MA-SAM/train.py
@@ -17,9 +17,9 @@ import os
 # os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3,4,5,6,7"
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--root_path', type=str, default='/root/data1/zmm/seg4medicine/data/BTCV', help='root dir for data')
-parser.add_argument('--output', type=str, default='/root/data1/zmm/seg4medicine/save/V7.2')
-parser.add_argument('--data_path', type=str, default='/root/data1/zmm/seg4medicine/data/BTCV')
+parser.add_argument('--root_path', type=str, default='/root/autodl-tmp/RawData/Training', help='root dir for data')
+parser.add_argument('--output', type=str, default='/root/autodl-tmp/save/V7.2')
+parser.add_argument('--data_path', type=str, default='/root/autodl-tmp/RawData/Training')
 parser.add_argument('--num_classes', type=int, default=12, help='output channel of network')
 parser.add_argument('--batch_size', type=int, default=7, help='batch_size per gpu')
 parser.add_argument('--n_gpu', type=int, default=8, help='total gpu')
@@ -32,7 +32,7 @@ parser.add_argument('--deterministic', type=int, default=1, help='whether use de
 parser.add_argument('--img_size', type=int, default=512, help='input patch size of network input')
 parser.add_argument('--seed', type=int, default=1234, help='random seed')
 parser.add_argument('--vit_name', type=str, default='vit_h', help='select one vit model')
-parser.add_argument('--ckpt', type=str, default='/root/data1/zmm/seg4medicine/pretrained/sam_vit_h_4b8939.pth', help='Pretrained checkpoint')
+parser.add_argument('--ckpt', type=str, default='/root/autodl-tmp/pretrained/sam_vit_h_4b8939.pth', help='Pretrained checkpoint')
 parser.add_argument('--adapt_ckpt', type=str, default=None, help='Finetuned checkpoint')
 parser.add_argument('--rank', type=int, default=32, help='Rank for FacT')
 parser.add_argument('--scale', type=float, default=1.0, help='Scale for FacT')
