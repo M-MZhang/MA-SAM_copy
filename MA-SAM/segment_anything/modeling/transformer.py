f06d1fafdc5e4d9d2de08d5baa3ceed1610969b9
M-MZhang <2211305270@qq.com>
2025-03-20 21:08:49 +0800

    有进步，改进是有效的，应该要为image增加这个attn

--- MA-SAM/segment_anything/modeling/transformer.py
+++ MA-SAM/segment_anything/modeling/transformer.py
@@ -59,6 +59,11 @@ class TwoWayTransformer(nn.Module):
         )
         self.norm_final_attn = nn.LayerNorm(embedding_dim)
 
+        self.final_image_self_attn = Attention(
+            embedding_dim, num_heads, downsample_rate=attention_downsample_rate
+        )
+        self.norm_final_image_attn = nn.LayerNorm(embedding_dim)
+
     def forward(
         self,
         image_embedding: Tensor,
@@ -102,6 +107,11 @@ class TwoWayTransformer(nn.Module):
         attn_out = self.final_attn_token_to_image(q=q, k=k, v=keys)
         queries = queries + attn_out
         queries = self.norm_final_attn(queries)
+        
+        q = keys + image_embedding
+        attn_out = self.final_image_self_attn(q=keys, k=q, v=q)
+        keys = keys + attn_out
+        keys = self.norm_final_image_attn(keys)
 
         return queries, keys
 
