af213cd0a35246889afe37a1da5395d487c09dab
M-MZhang <2211305270@qq.com>
2025-04-10 23:03:11 +0800

    logger debug

--- MA-SAM/trainer.py
+++ MA-SAM/trainer.py
@@ -267,7 +267,7 @@ def trainer_run(args, model, snapshot_path, multimask_output, low_res):
                 model.module.save_parameters(save_mode_path)
                 # torch.save(model.module.state_dict(), save_mode_path)
             logger.info("save model to {}".format(save_mode_path))
-            dice = inference_2d(args, multimask_output, model,  low_res, None)
+            dice = inference_2d(args, multimask_output, model,  low_res, logger, None)
             if dice > best_dice:
                 best_dice = dice
                 save_mode_path = os.path.join(snapshot_path, 'best.pth')
