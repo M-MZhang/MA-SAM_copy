1a6d6722a270a8a5c1d63d045b57ae88fd462030
M-MZhang <2211305270@qq.com>
2025-04-20 17:20:45 +0800

    prompt number ablation

--- MA-SAM/train.sh
+++ MA-SAM/train.sh
@@ -1,26 +1,29 @@
 TRAINER="HSP-SAM"
 
 DATASET=("isic2018" "CVC-ClinicDB" "UDIAT" "dsb-2018" )
+prompt_num=(1 2 4 8 16)
 ROOT_PATH="/root/autodl-tmp/data"
 OUTPUT_PATH="/root/autodl-tmp/save/${TRAINER}"
-CONFIG="lr_0.0012_weight_decay_0.1_augmentation_True"
+CONFIG="lr_0.0012_weight_decay_0.1"
 
 for dataset in ${DATASET[@]}
-do
-    DATA_PATH="${ROOT_PATH}/${dataset}"
-    OUTPUT="${OUTPUT_PATH}/${dataset}/${CONFIG}"
-    VISUAL_PATH="/root/autodl-tmp/visualization/${TRAINER}/${dataset}"
+    for prompt in ${prompt_num[@]}
+    do
+        DATA_PATH="${ROOT_PATH}/${dataset}"
+        OUTPUT="${OUTPUT_PATH}/${dataset}/${CONFIG}/${prompt}"
+        VISUAL_PATH="/root/autodl-tmp/visualization/${TRAINER}/${dataset}"
 
-    python train.py \
-        --data_path=${DATA_PATH} \
-        --output=${OUTPUT} \
-        --visual_path=${VISUAL_PATH} \
-        --root_path=${DATA_PATH} \
-        --num_classes=1 \
-        --batch_size=20 \
-        --n_gpu=2 \
-        --base_lr=0.0012 \
-        --weight_decay=0.1 \
-        --vit_name='vit_h' \
-        --ckpt='/root/autodl-tmp/pretrained/sam_vit_h_4b8939.pth' 
-done
+        python train.py \
+            --num_classes=${prompt} \
+            --data_path=${DATA_PATH} \
+            --output=${OUTPUT} \
+            --visual_path=${VISUAL_PATH} \
+            --root_path=${DATA_PATH} \
+            --num_classes=1 \
+            --batch_size=20 \
+            --n_gpu=2 \
+            --base_lr=0.0012 \
+            --weight_decay=0.1 \
+            --vit_name='vit_h' \
+            --ckpt='/root/autodl-tmp/pretrained/sam_vit_h_4b8939.pth' 
+    done
