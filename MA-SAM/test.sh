fe81d33733b702fd2eb68ef22943d1f69b1ca717
M-MZhang <2211305270@qq.com>
2025-04-20 21:06:14 +0800

    save the best only

--- MA-SAM/test.sh
+++ MA-SAM/test.sh
@@ -1,10 +1,9 @@
 TRAINER="HSP-SAM"
 
 DATASET=("DRIVE" "CVC-ClinicDB" "UDIAT" "dsb-2018" "isic2018")
-# DATASET="dsb-2018"
 ROOT_PATH="/root/autodl-tmp/data"
 OUTPUT_PATH="/root/autodl-tmp/save/${TRAINER}"
-CONFIG="lr_0.0012_weight_decay_0.1"
+CONFIG="lr_0.0012_weight_decay_0.1_ablation_1"
 
 
 for dataset in ${DATASET[@]}
@@ -14,7 +13,7 @@ do
     adapt_ckpt="${OUTPUT}/best.pth"
     VISUAL_PATH="/root/autodl-tmp/visualization/${TRAINER}/${dataset}"
 
-    python test.py \
+    CUDA_VISIBLE_DEVICES=2,3 python test.py \
         --data_path=${DATA_PATH} \
         --output_dir=${OUTPUT} \
         --visual_path=${VISUAL_PATH} \
@@ -24,5 +23,4 @@ do
         --vit_name='vit_h' \
         --ckpt='/root/autodl-tmp/pretrained/sam_vit_h_4b8939.pth'\
         --adapt_ckpt=${adapt_ckpt} 
-
 done
