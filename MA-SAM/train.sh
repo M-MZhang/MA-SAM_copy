4ff532e8fb6ac41149989c2e22d7251ad1beacf0
M-MZhang <2211305270@qq.com>
2025-04-21 15:13:12 +0800

    lr=0.0008

--- MA-SAM/train.sh
+++ MA-SAM/train.sh
@@ -23,7 +23,7 @@ do
             --num_classes=1 \
             --batch_size=20 \
             --n_gpu=2 \
-            --base_lr=0.0012 \
+            --base_lr=0.0008 \
             --weight_decay=0.1 \
             --vit_name='vit_h' \
             --ckpt='/root/autodl-tmp/pretrained/sam_vit_h_4b8939.pth' 
